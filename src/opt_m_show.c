bfd52a6a8773f4bee6178b83548acc7913909997
Tristan PAYEN <tpayen@student.42.fr>
2015-12-17 19:22:06 +0100

    Add -G for -m

--- src/opt_m_show.c
+++ src/opt_m_show.c
@@ -6,7 +6,7 @@
 /*   By: tpayen <tpayen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/15 17:00:02 by tpayen            #+#    #+#             */
-/*   Updated: 2015/12/15 17:49:53 by tpayen           ###   ########.fr       */
+/*   Updated: 2015/12/17 19:21:31 by tpayen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	opt_m_show(t_list *lst)
 {
-	ft_putstr(g_name(lst));
+	ft_putstr(show_color(lst));
 	if (can(OPT_P) && S_ISDIR(g_stat(lst)->st_mode))
 		ft_putchar('/');
 	if (lst->next)
